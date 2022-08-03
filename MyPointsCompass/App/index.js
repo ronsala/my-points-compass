a1ea0dc65706faaa2747b0e59a9fa824c37fd884
RonSala <ron.sala@gmail.com>
2022-08-03 05:23:32 -0400

    separate code

---
+++ MyPointsCompass/App/index.js
@@ -0,0 +1,25 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import CompassContainer from "./containers/CompassContainer";
+
+export default function App() {
+	return (
+		<View style={styles.container}>
+			<Text>My Points Compass</Text>
+			<Text></Text>
+			<CompassContainer />
+
+			<StatusBar hidden={false} />
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+});
